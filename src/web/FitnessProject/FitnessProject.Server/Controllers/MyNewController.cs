8d10452a7b33b05625bf6062925c8bf9a687795c
ykosbie <ykosbie@sorissoftware.com>
2019-08-15 21:39:57 +0300

    Add new sample controller

---
+++ src/web/FitnessProject/FitnessProject.Server/Controllers/MyNewController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FitnessProject.Server.Controllers
+{
+    public class MyNewController
+    {
+    }
+}
