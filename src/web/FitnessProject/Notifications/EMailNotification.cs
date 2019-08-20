c2fd36a40231f4bae21fef6acaf001743915eb4e
Elia Sternbuch <sternbuch.elijahu@hotmail.ch>
2019-08-20 12:24:40 +0200

    added sms nortifaktions

--- src/web/FitnessProject/Notifications/EMailNotification.cs
+++ src/web/FitnessProject/Notifications/EMailNotification.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.Net;
 using System.Net.Mail;
 
@@ -6,7 +7,7 @@ namespace Notifications
 {
     public static class EMailNotification
     {
-        public static void send(string To, string Sub, string Message)
+        public static void Send(string To, string Sub, string Message)
         {
             var fromAddress = new MailAddress("compuskills.fitnessprojekt@gmail.com", "Compuskills Capstone");
             var toAddress = new MailAddress(To);
@@ -29,6 +30,23 @@ namespace Notifications
             })
             {
                 smtp.Send(message);
+            }
+        }
+    }
+    //SMS Api Using https://control.txtlocal.co.uk/docs/ Login: qcr57223@oqiwq.com Password:3412534125Cc
+    public static class SMSNotification
+    {
+        public static void Send(string ToNuber, string Message)
+        {
+            using (var wb = new WebClient())
+            {
+                byte[] response = wb.UploadValues("https://api.txtlocal.com/send/", new NameValueCollection()
+                {
+                {"apikey" , "n6131WEXd5Y-0b0KBG44o7r4QmD25gv3GkwgmXSBeW"},
+                {"numbers" , ToNuber},
+                {"message" , Message},
+                {"sender" , "Capstone Projekt"}
+                });
             }
         }
     }
