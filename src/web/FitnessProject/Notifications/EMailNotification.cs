01492d148aef02635e04617c13b16fc9f052d6e8
Elia Sternbuch <sternbuch.elijahu@hotmail.ch>
2019-09-02 15:44:30 +0200

    added push with firebase and added suppurt to INortifakitions

--- src/web/FitnessProject/Notifications/EMailNotification.cs
+++ src/web/FitnessProject/Notifications/EMailNotification.cs
@@ -1,17 +1,33 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Specialized;
+using System.IO;
 using System.Net;
 using System.Net.Mail;
+using System.Text;
 
 namespace Notifications
 {
-    public static class EMailNotification
+    public class EMailNotification :INotifications
     {
-        public static void Send(string To, string Sub, string Message)
+        public EMailNotification():this("compuskills.fitnessprojekt@gmail.com", "Compuskills Capstone", "3412534125")
+        { }
+        public EMailNotification(string senderEmail, string senderName, string senderPassword)
         {
-            var fromAddress = new MailAddress("compuskills.fitnessprojekt@gmail.com", "Compuskills Capstone");
-            var toAddress = new MailAddress(To);
-            const string fromPassword = "3412534125";
+            SenderEmail = senderEmail;
+            SenderName = senderName;
+            SenderPassword = senderPassword;
+        }
+
+        public string SenderEmail { get; set; }
+        public string SenderName { get; set; }
+        public string SenderPassword { get; set; }
+
+        
+        public void Send(MessageData messageData)
+        {
+            var fromAddress = new MailAddress(SenderEmail, SenderName);
+            var toAddress = new MailAddress(messageData.EMail);
 
 
             var smtp = new SmtpClient
@@ -21,33 +37,105 @@ namespace Notifications
                 EnableSsl = true,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
                 UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                Credentials = new NetworkCredential(fromAddress.Address, SenderPassword)
             };
             using (var message = new MailMessage(fromAddress, toAddress)
             {
-                Subject = Sub,
-                Body = Message
+                Subject = messageData.MsgHeader,
+                Body = messageData.MsgBody
             })
             {
                 smtp.Send(message);
             }
+            smtp.Dispose();
         }
     }
     //SMS Api Using https://control.txtlocal.co.uk/docs/ Login: qcr57223@oqiwq.com Password:3412534125Cc
-    public static class SMSNotification
+    public class SMSNotification : INotifications
     {
-        public static void Send(string ToNuber, string Message)
+        public SMSNotification():this("n6131WEXd5Y-0b0KBG44o7r4QmD25gv3GkwgmXSBeW", "Capstone Projekt")
+        {
+        }
+        public SMSNotification(string apiKey, string senderName)
+        {
+            ApiKey = apiKey;
+            SenderName = senderName;
+        }
+
+        public string ApiKey { get; set; }
+        public string SenderName { get; set; }
+        public void Send(MessageData messageData)
         {
             using (var wb = new WebClient())
             {
                 byte[] response = wb.UploadValues("https://api.txtlocal.com/send/", new NameValueCollection()
                 {
-                {"apikey" , "n6131WEXd5Y-0b0KBG44o7r4QmD25gv3GkwgmXSBeW"},
-                {"numbers" , ToNuber},
-                {"message" , Message},
-                {"sender" , "Capstone Projekt"}
+                {"apikey" , ApiKey},
+                {"numbers" , messageData.TelNr},
+                {"message" , messageData.MsgHeader+" "+messageData.MsgBody},
+                {"sender" , SenderName}
                 });
             }
         }
+    }
+    public class PushFirebase : INotifications
+    {
+        public PushFirebase()
+        {
+        }
+        public PushFirebase(string serverKey, string senderID)
+        {
+            ServerKey = serverKey;
+            SenderID = senderID;
+        }
+
+        //serverKey - Key from Firebase cloud messaging server  
+        public string ServerKey { get; set; }
+
+        //Sender Id - From firebase project setting  
+        public string SenderID { get; set; }
+
+        public void Send(MessageData messageData)
+        {
+            WebRequest tRequest = WebRequest.Create("https://fcm.googleapis.com/fcm/send");
+            tRequest.Method = "post";
+            tRequest.Headers.Add(string.Format("Authorization: key={0}", ServerKey));
+            tRequest.Headers.Add(string.Format("Sender: id={0}", SenderID));
+            tRequest.ContentType = "application/json";
+            var msg = new
+            {
+                to = messageData.PushID,
+                priority = "high",
+                content_available = true,
+                notification = new
+                {
+                    body = messageData.MsgHeader,
+                    title = messageData.MsgHeader,
+                    badge = 1
+                },
+            };
+
+            string postbody = JsonConvert.SerializeObject(msg).ToString();
+            Byte[] byteArray = Encoding.UTF8.GetBytes(postbody);
+            tRequest.ContentLength = byteArray.Length;
+
+            using (Stream dataStream = tRequest.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                using (WebResponse tResponse = tRequest.GetResponse())
+                {
+                    using (Stream dataStreamResponse = tResponse.GetResponseStream())
+                    {
+                        if (dataStreamResponse != null) using (StreamReader tReader = new StreamReader(dataStreamResponse))
+                            {
+                                String sResponseFromServer = tReader.ReadToEnd();
+                                //result.Response = sResponseFromServer;
+                            }
+                    }
+                }
+            }
+        }
+
+
     }
 }
---
+++ src/web/FitnessProject/Notifications/MessageData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notifications
+{
+    public class MessageData
+    {
+        public string PushID { get; set; }
+        public string EMail { get; set; }
+        private string _TelNr;
+
+        public string TelNr
+        {
+            get { return _TelNr; }
+            set {
+                if (value[0]=='0')
+                {
+                    if (value[1]=='0')
+                    {
+                        _TelNr=value.Substring(2);
+                    }
+                    else
+                    {
+                        _TelNr = value.Substring(1);
+                    }
+                }
+                else { _TelNr = value; }
+               
+            }
+        }
+
+        public string MsgHeader { get; set; }
+        public string MsgBody { get; set; }
+    }
+}
