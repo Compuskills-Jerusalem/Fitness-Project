e4b06a5fe27b5e20a171738220e895ba75424057
Yehrman <46247567+Yehrman@users.noreply.github.com>
2019-09-05 21:34:05 +0300

    Adding address geoLocation capability

--- src/ServerCode/FitnessProjectServerSide/Controllers/LocationController.cs
+++ src/ServerCode/FitnessProjectServerSide/Controllers/LocationController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -46,3 +46,5 @@ namespace WebApplication1.Controllers
                 return View(status);
         }
     }
+
+}
