c402a9493170ddc3e80d2348d8b3db2ea357aef9
Elia Sternbuch <sternbuch.elijahu@hotmail.ch>
2019-09-18 10:15:03 +0200

    added pushid recever in server

--- src/gui/PN_XAML/PN_XAML.Android/MyFirebaseIIDService.cs
+++ src/gui/PN_XAML/PN_XAML.Android/MyFirebaseIIDService.cs
@@ -2,6 +2,8 @@
 using Android.App;
 using Firebase.Iid;
 using Android.Util;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace PN_XAML.Droid
 {
@@ -14,6 +16,28 @@ namespace PN_XAML.Droid
         {
             var refreshedToken = FirebaseInstanceId.Instance.Token;
             Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+            SendToServerAsync(refreshedToken);
+        }
+        public async System.Threading.Tasks.Task SendToServerAsync(string token)
+        {
+
+            IEnumerable<KeyValuePair<string, string>> queries = new List<KeyValuePair<string, string>>()
+            {
+                {new KeyValuePair<string, string>(key: "Token", value: token) },
+            };
+            FormUrlEncodedContent q = new FormUrlEncodedContent(queries);
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.PostAsync("http://www.compuskillscapstoneprojekt.com", q))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string myContent = await content.ReadAsStringAsync();
+                        Console.WriteLine(myContent);
+                    }
+                }
+            }
+
         }
     }
 }
--- src/web/FitnessProject.Server/FitnessProjectServerSide/Controllers/HomeController.cs
+++ src/web/FitnessProject.Server/FitnessProjectServerSide/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using FitnessProjectServerSide.Models;
+using Notifications;
 
 namespace FitnessProjectServerSide.Controllers
 {
@@ -46,8 +47,27 @@ namespace FitnessProjectServerSide.Controllers
         {
             Geolocation PersonsLocation = new Geolocation() { Latitude = Latitude, Longitude = Longitude };
             //GpsSensor gpsSensor = new GpsSensor(PersonsLocation);
-           // MessageRelayer relayer = new MessageRelayer();
-           // relayer.RelayMessage(gpsSensor);
+            // MessageRelayer relayer = new MessageRelayer();
+            // relayer.RelayMessage(gpsSensor);
+            
+            EMailNotification n = new EMailNotification();
+            MessageData messageData = new MessageData()
+            {
+                EMail = "ykosbie@compuskills.org",
+                MsgBody = string.Format("The Latitude is {0} and the Longitude is {1}.", PersonsLocation.Latitude, PersonsLocation.Longitude),
+                MsgHeader = "Test is Working",
+                TelNr = "972586846003"
+            };
+            n.Send(messageData);
+            SMSNotification s = new SMSNotification();
+            s.Send(messageData);
+            
+        }
+        [HttpPost]
+        public void SenderID(string Token)
+        {
+            string PushID = Token;
+            //todo: send token to db
 
         }
 
