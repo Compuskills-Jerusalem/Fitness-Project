841283d32d63cb1e295e868e663f12598579f098
Yehrman <46247567+Yehrman@users.noreply.github.com>
2019-09-19 00:46:59 +0300

    Update NoGoZone.cs

--- src/web/FitnessProject.Server/FitnessProjectServerSide/Models/NoGoZone.cs
+++ src/web/FitnessProject.Server/FitnessProjectServerSide/Models/NoGoZone.cs
@@ -4,7 +4,7 @@ namespace FitnessProjectServerSide.Models
     public class NoGoZone
     {
         public int id { get; set; }
-        public ICollection<UserNoGoZone> userNos { get; set; }
+        public ICollection<UserNoGoZone> UserNoGoZones { get; set; }
         public string Address { get; set; }
         public double Laditude { get; set; }
         public double Longitude { get; set; }
