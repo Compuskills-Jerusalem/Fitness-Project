fcdeca65b0bc2e2dcc36e5a2f440d5c57c6fa378
Yehrman <46247567+Yehrman@users.noreply.github.com>
2019-09-22 18:43:08 +0300

    updates

--- src/web/FitnessProject.Server/FitnessProjectServerSide/Controllers/HomeController.cs
+++ src/web/FitnessProject.Server/FitnessProjectServerSide/Controllers/HomeController.cs
@@ -10,49 +10,18 @@ using Notifications;
 namespace FitnessProjectServerSide.Controllers
 {
     public class HomeController : Controller
-    { public ActionResult Home()
+    {
+        public ActionResult index()
         {
             ViewBag.Title = "Home Page";
             return View();
         }
+
+      
+      
+        
      
-        [HttpGet]
-      public ActionResult Create()
-        {
-            
-            return View();
-        }
-        [HttpPost]
-        public ActionResult AddUser(string name)
-        {
-            using (FittAppContext fitt = new FittAppContext())
-            {
-                
-                fitt.Users.Add(new User {Name=name});
-                fitt.SaveChanges();
-            }
-            return RedirectToAction("GetAddress","Location");
-        }
-        [HttpGet]
-        public ActionResult Login(int id,int noGoid)
-        {
-         
-                return View();
-        }
-        [HttpGet]
-        public ActionResult UserInfo()
-        {
-            using (FittAppContext fitt = new FittAppContext())
-            {
-                var a = fitt.UserNoGoZones.Take(4).Include(x => x.User).Select(y => y.NoGoZone);
-                foreach (var item in a)
-                {
-                    var B = item.UserNoGoZones.AsEnumerable();
-                    return View(B);
-                }
-            }
-            return View();
-        }
+       
         [HttpPost]
         public void RelayMessage(double Latitude, double Longitude)
         {
---
+++ src/web/FitnessProject.Server/FitnessProjectServerSide/Controllers/UserController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FitnessProjectServerSide.Models;
+
+namespace FitnessProjectServerSide.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            using (FittAppContext fitt = new FittAppContext())
+            {
+                return View(fitt.Users);
+            }
+                
+        }
+
+        /* GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        */
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddUser(string name)
+        {
+            try
+            {
+                using (FittAppContext fitt = new FittAppContext())
+                {
+
+                    fitt.Users.Add(new User { Name = name });
+                    fitt.SaveChanges();
+                }
+                return RedirectToAction("GetAddress", "Location");
+            }
+            catch
+            {             
+            }
+            return View();
+        }
+
+        // GET: User/Edit/5
+     /*   public ActionResult Edit(int id)
+        {
+            using (FittAppContext fitt = new FittAppContext())
+            {
+                var model = fitt.UserNoGoZones.FirstOrDefault(x => x.users.id == id)
+               return View(model);
+            }
+            
+        }
+
+        // POST: User/Edit/5
+       [HttpPost]
+        public ActionResult Edit(int id, NoGoZone noGo)
+        {
+            try
+            {
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }*/
+    }
+}
