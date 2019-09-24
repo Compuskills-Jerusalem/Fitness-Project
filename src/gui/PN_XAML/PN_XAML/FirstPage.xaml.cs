30758204ddceff9cee1f2f3c4952fa4de401086f
348959034 <46166782+348959034@users.noreply.github.com>
2019-09-24 12:59:57 +0300

    Instead of sending the users current location once the app now sends the user location every 10 seconds

--- src/gui/PN_XAML/PN_XAML/FirstPage.xaml.cs
+++ src/gui/PN_XAML/PN_XAML/FirstPage.xaml.cs
@@ -14,6 +14,7 @@ using System.Net.Sockets;
 using System.Net.Http.Headers;
 using System.Net.Http;
 //using Org.Apache.Http.Protocol;
+using System.Threading;
 
 namespace PN_XAML
 {
@@ -22,55 +23,52 @@ namespace PN_XAML
         public FirstPage()
         {
             InitializeComponent();
+
         }
         protected override void OnAppearing()
         {
             base.OnAppearing();
         }
+
+
         async void btnLocation_Clicked(object sender, System.EventArgs e)
         {
-            try
+
+
+            while (true)
             {
-                var location = await Geolocation.GetLastKnownLocationAsync();
-                
-                //Location jrusalem = new Location(31.7683, 35.2137);
-                //double distance = Xamarin.Essentials.Location.CalculateDistance(location, jrusalem, DistanceUnits.Kilometers);
-                if (location != null)
+                try
                 {
-                    int userID = 1;
-                    //Ben can change the first perameter to the projects server so it recieves the http message
-                    PostRequest("http://10.0.2.2:55588/MobileGPS/RelayMessage", location.Latitude.ToString(), location.Longitude.ToString(), userID.ToString());
+                    var location = await Geolocation.GetLastKnownLocationAsync();
 
-                    //    lblLatitude.Text = "Your Latitude: " + location.Latitude.ToString();
-                    //    lblLongitude.Text = "Your Longitude:" + location.Longitude.ToString();
-                    //}
-                    //if (jrusalem != null)
-                    //{
-                    //    jrlblLatitude.Text = "Jr Latitude: " + jrusalem.Latitude.ToString();
-                    //    jrlblLongitude.Text = "Jr Longitude:" + jrusalem.Longitude.ToString();
-                    //}
 
-                    //if (distance < 25)
-                    //{
-                    //    lblAlert.Text = ("you are being alerted because your distance from Jeruselum is less than 25 km, your current distance from jr is "+ Math.Round(distance)+" km");
-                    lblConfirmation.Text = "You are not set up to recieve notification";
+                    if (location != null)
+                    {
+                        int userID = 1;
+
+                        PostRequest("http://10.0.2.2:55588/MobileGPS/RelayMessage", location.Latitude.ToString(), location.Longitude.ToString(), userID.ToString());
+                        lblConfirmation.Text = "You are not set up to recieve notification";
+                    }
+
 
                 }
+                catch (FeatureNotSupportedException fnsEx)
+                {
+                    await DisplayAlert("Faild", fnsEx.Message, "OK");
+                }
+                catch (PermissionException pEx)
+                {
+                    await DisplayAlert("Faild", pEx.Message, "OK");
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Faild", ex.Message, "OK");
+                }
+            Thread.Sleep(10000);
+            }
 
-            }
-            catch (FeatureNotSupportedException fnsEx)
-            {
-                await DisplayAlert("Faild", fnsEx.Message, "OK");
-            }
-            catch (PermissionException pEx)
-            {
-                await DisplayAlert("Faild", pEx.Message, "OK");
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Faild", ex.Message, "OK");
-            }
 
+            
         }
         async static void PostRequest(string url, string latitude, string longitude, string userId)
         {
@@ -83,7 +81,7 @@ namespace PN_XAML
             FormUrlEncodedContent q = new FormUrlEncodedContent(queries);
             using (HttpClient client = new HttpClient())
             {
-                using (HttpResponseMessage response = await client.PostAsync(url,q))
+                using (HttpResponseMessage response = await client.PostAsync(url, q))
                 {
                     using (HttpContent content = response.Content)
                     {
