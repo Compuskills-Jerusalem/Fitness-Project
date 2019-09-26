c409bccf15dcc6daee6feb7b2f552cbb849ea9e4
wolfbear77 <zevdovklein@gmail.com>
2019-09-26 22:12:37 +0300

    Merge

--- src/web/FitnessProject.Web.Mvc/Models/UserInfo.cs
+++ src/web/FitnessProject.Web.Mvc/Models/UserInfo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace FitnessProjectServerSide.Models
+namespace FitnessProject.Web.Mvc
 {
     public class UserInfoModel
     {
