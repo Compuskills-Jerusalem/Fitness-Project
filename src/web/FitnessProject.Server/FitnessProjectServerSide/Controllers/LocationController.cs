e25380c268ebae0823954171e7b5da04dbe66790
Yehrman <46247567+Yehrman@users.noreply.github.com>
2019-09-26 15:02:03 +0300

    Added userQuery

--- src/web/FitnessProject.Server/FitnessProjectServerSide/Controllers/LocationController.cs
+++ src/web/FitnessProject.Server/FitnessProjectServerSide/Controllers/LocationController.cs
@@ -35,20 +35,7 @@ namespace FitnessProjectServerSide.Controllers
         [HttpPost]
         public ActionResult UserQueryResult(string name,NoGoZone noGo)
         {
-            using (var fitt = new FittAppContext())
-            {
-               
-                //  e model item passed into the dictionary is of type 'System.Data.Entity.Infrastructure.DbQuery`1[<>f__AnonymousType3`3[System.String,System.Double,System.Double]]', but this dictionary requires a model item of type 'System.Collections.Generic.IEnumerable`1[FitnessProjectServerSide.Models.NoGoZone]'.
-                var model = from person in fitt.UserNoGoZones
-                            where person.users.Name == name
-                            select new
-                            {
-                                person.NoGoZones.Address,
-                               person.NoGoZones.Laditude,
-                                person.NoGoZones.Longitude,
-                               
-                            };
-                return View(model.ToList());
+           
 
             }
            
