bf4859d31f66cc07cc0a24ae889837b66874e6ad
mynumenisbinyumin <binyominyucht@gmail.com>
2019-11-06 22:13:39 +0200

    i dont know

--- src/web/FitnessProject.Web.Mvc/Controllers/GpsSensorController.cs
+++ src/web/FitnessProject.Web.Mvc/Controllers/GpsSensorController.cs
@@ -7,6 +7,7 @@ using System.Data.Entity;
 using FitnessProject.Web.Mvc;
 using DatabaseConn;
 using FitnessProject.Web.Notifications;
+using Xamarin.Essentials;
 
 namespace FitnessProject.Web.Mvc.Controllers
 {
@@ -21,18 +22,19 @@ namespace FitnessProject.Web.Mvc.Controllers
                               select new { UserContact.User.UserID }).Single();
 
                 var UserSensorID = (from UserSensor in FAC.UserSensors
-                                   where UserSensor.UserID == userID.UserID && UserSensor.Sensor.SensorName == "GpsSensor"
-                                   select new { UserSensor.UserSensorID }).Single();
+                                    where UserSensor.UserID == userID.UserID && UserSensor.Sensor.SensorName == "GpsSensor"
+                                    select new { UserSensor.UserSensorID }).Single();
 
                 var TargetListItems = from UserSensorAlert in FAC.UserSensorAlerts
                                       where UserSensorAlert.UserSensorID == UserSensorID.UserSensorID
                                       select UserSensorAlert.UserSensorAlertID;
 
                 MessageTargetList List = new MessageTargetList();
-                foreach ( var TargetListItem in TargetListItems)
+                foreach (var TargetListItem in TargetListItems)
                 {
                     if (TargetListItem == 1)
                     {
+                        MessageData messageData = new MessageData();
                         EMailNotification eMailNotification = new EMailNotification();
                         List.MessageTypeTargetList.Add(eMailNotification);
                     }
@@ -42,6 +44,21 @@ namespace FitnessProject.Web.Mvc.Controllers
             }
 
         }
+
+        public IQueryable<NoGoZones> GetNoGoZonesByEmail(string EmailAddress)
+        {
+            using (FittAppContext FAC = new FittAppContext())
+            {
+                var noGoZones = from userContact in FAC.UserContacts
+                                where userContact.ContactValue == EmailAddress
+                                from userNoGoZone in userContact.User.UserNoGoZones
+                                select userNoGoZone.NoGoZone;
+
+                return noGoZones.ToList().AsQueryable();
+            }
+        }
+
+
         //what you have to do above is to get the EmailAddress string query the corresponding users ID and then query all of his Alerts for Gps
         //then translate that to a message target list somehow 
         //then your going to have to provide that to the message relayer in the relay action method
@@ -52,21 +69,31 @@ namespace FitnessProject.Web.Mvc.Controllers
         }
 
         [HttpPost]
-        public void RelayMessage(Geolocation PersonsLocation, string EmailAddress)
+        public void RelayMessage(double latitude, double longitude, string emailAddress)
         {
-            MessageTargetList targetList = GpsSensorTargetList(EmailAddress);
-            GpsSensor gpsSensor = new GpsSensor(PersonsLocation);
+
+            Xamarin.Essentials.Location PersonsLocation = new Xamarin.Essentials.Location(latitude, longitude);
+
+            MessageTargetList targetList = GpsSensorTargetList(emailAddress);
+            GpsSensor gpsSensor = new GpsSensor(PersonsLocation, GetNoGoZonesByEmail(emailAddress));
             bool signal = gpsSensor.ShouldAlertUser();
             if (signal == true)
             {
-                foreach ( var item in targetList.MessageTypeTargetList)
+                foreach (var item in targetList.MessageTypeTargetList)
                 {
-                    item.Send();
+                    item.Send(new MessageData
+                    {
+                        EMail = emailAddress,
+                        MsgBody = "Success",
+                        MsgHeader = "taaaaaaaaaaaaatttttttttiiiiiiiiii",
+                        TelNr = "972586846003"
+
+                    });
                 }
             }
             //MessageRelayer relayer = new MessageRelayer();
             //relayer.RelayMessage(gpsSensor);
-            
+
         }
         [HttpGet]
         public void Mock()
--- src/web/FitnessProject.Web.Mvc/GpsSensor.cs
+++ src/web/FitnessProject.Web.Mvc/GpsSensor.cs
@@ -1,19 +1,44 @@
-﻿using FitnessProject.Web.Mvc.Interfaces;
+﻿using DatabaseConn;
+using FitnessProject.Web.Mvc.Interfaces;
+using System.Linq;
+using Xamarin.Essentials;
 
 namespace FitnessProject.Web.Mvc
 {
     public class GpsSensor : ISensor
     {
+        const double THRESHOLD = 0.1;
+
         public int SensorID { get; set; }
+        public bool DistanceIsLessThanThreshold { get; set; }
 
         public bool ShouldAlertUser()
         {
-            return true;
+            return DistanceIsLessThanThreshold;
         }
 
-        public GpsSensor(Geolocation PersonsLocation )
+        public GpsSensor(Location personsLocation, IQueryable<NoGoZones> noGoZones)
         {
+            //public static void CalcPlace(IEnumerable<NoGoZone> NoGoUsers, Location User)
+            //{sh
 
+            foreach (var noGoZoneItem in noGoZones)
+            {
+                Location DZone = new Location(noGoZoneItem.Latitude, noGoZoneItem.Longitude);
+                var distance = Location.CalculateDistance(personsLocation, DZone, DistanceUnits.Kilometers);
+                if (distance < THRESHOLD)
+                {
+                    DistanceIsLessThanThreshold = true;
+
+                }
+                else
+                {
+                    DistanceIsLessThanThreshold = false;
+                }
+
+
+            }
         }
     }
+}
 }
