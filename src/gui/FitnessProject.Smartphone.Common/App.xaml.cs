6b0f8ba82f050b99f01434bb6228da79d6c6ff00
Yitzchak93 <yitzchakhorowitz321@gmail.com>
2019-11-06 16:10:38 +0200

    added login page, added 2 Parameter post request when user enters username and password

--- src/gui/FitnessProject.Smartphone.Common/App.xaml.cs
+++ src/gui/FitnessProject.Smartphone.Common/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PN_XAML.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace PN_XAML
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new LoginPage()/*,new MainPage()*/);
         }
 
         protected override void OnStart()
